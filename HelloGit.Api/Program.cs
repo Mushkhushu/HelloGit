9d944463ffc2ebb1fb1b80757ac26c48e239be7b
Mushkhushu <flavie.houlette@laposte.net>
2025-06-21 19:24:05 +0200

    first try to seed the db, added appsettings files to cs.proj (api)

--- HelloGit.Api/Program.cs
+++ HelloGit.Api/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Microsoft.Extensions.Configuration;
+using HelloGit.Api.Services;
+using HelloGit.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelloGit.Api
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddJsonFile("appsettings.Development.json", optional: false)
+                .Build();
+
+            string gitHubToken = configuration["GitHubToken"];
+
+            var gitHubClient = new GitHubApiClient(gitHubToken);
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlite("Data Source=hellogit.db");
+
+            using var dbContext = new AppDbContext(optionsBuilder.Options);
+            await dbContext.Database.EnsureCreatedAsync();
+
+            var seeder = new DatabaseSeeder(gitHubClient, dbContext);
+            await seeder.SeedAsync();
+
+
+            Console.WriteLine("Seed terminé.");
+        }
+    }
+}
